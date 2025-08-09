8483a46ab986c7cd6314d303bda7f3648af9b6e8
sarahsululu <sarahsululu47@gmail.com>
2025-08-09 17:11:44 +0300

    all passing tests

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length == 0:
+        print([])
+        return
+    elif length < 0:
+        print("Length must be a positive integer.")
+        return
+    
+    sequence = [0, 1]
+    while len(sequence) < length:
+        sequence.append(sequence[-1] + sequence[-2])
+    print(sequence[:length])
